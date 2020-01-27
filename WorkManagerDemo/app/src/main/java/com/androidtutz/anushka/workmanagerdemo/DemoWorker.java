fa5453d9ea65453b8b206c37033ed7cc7425b6c8
artur.rodrigues <artur.rodrigues@tivit.com>
2020-01-27 10:42:02 -0300

    Commit final WorkManager

--- WorkManagerDemo/app/src/main/java/com/androidtutz/anushka/workmanagerdemo/DemoWorker.java
+++ WorkManagerDemo/app/src/main/java/com/androidtutz/anushka/workmanagerdemo/DemoWorker.java
@@ -2,9 +2,14 @@ package com.androidtutz.anushka.workmanagerdemo;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.util.Log;
 
+import androidx.work.Data;
 import androidx.work.Worker;
 import androidx.work.WorkerParameters;
+
+import static com.androidtutz.anushka.workmanagerdemo.MainActivity.KEY_COUNT_VALUE;
+import static com.androidtutz.anushka.workmanagerdemo.MainActivity.KEY_RETURN_VALUE;
 
 public class DemoWorker extends Worker {
 
@@ -15,6 +20,18 @@ public class DemoWorker extends Worker {
     @NonNull
     @Override
     public Result doWork() {
-        return null;
+
+        Data data = getInputData();
+        int count = data.getInt(KEY_COUNT_VALUE, 0);
+
+        for (int i = 0; i < count; i++) {
+            Log.i("MYTAG", "Count is " + i);
+        }
+
+        Data response = new Data.Builder()
+                .putString(KEY_RETURN_VALUE, "Deu boa!!!")
+                .build();
+
+        return Result.success(response);
     }
 }
--- WorkManagerDemo/app/src/main/java/com/androidtutz/anushka/workmanagerdemo/MainActivity.java
+++ WorkManagerDemo/app/src/main/java/com/androidtutz/anushka/workmanagerdemo/MainActivity.java
@@ -1,48 +1,75 @@
 package com.androidtutz.anushka.workmanagerdemo;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.work.Constraints;
+import androidx.work.Data;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkManager;
+
+import com.androidtutz.anushka.workmanagerdemo.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    public final static String KEY_COUNT_VALUE = "key_count";
+    public final static String KEY_RETURN_VALUE = "key_return";
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        setSupportActionBar(binding.toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        Data data = new Data.Builder()
+                .putInt(KEY_COUNT_VALUE, 30000)
+                .build();
+
+        Constraints constraints = new Constraints.Builder()
+                .setRequiresCharging(true)
+                .build();
+
+        final OneTimeWorkRequest request = new OneTimeWorkRequest.Builder(DemoWorker.class)
+                .setInputData(data)
+                .setConstraints(constraints)
+                .build();
+
+        binding.fab.setOnClickListener(view -> WorkManager
+                .getInstance().enqueue(request));
+
+        WorkManager.getInstance()
+                .getWorkInfoByIdLiveData(request.getId())
+                .observe(this, workInfo -> {
+                    if(workInfo != null) {
+                        binding.content.tvStatus.setText(workInfo.getState().name());
+
+                        if(workInfo.getState().isFinished()) {
+                            Data data1 = workInfo.getOutputData();
+
+                            Toast.makeText(this, data1.getString(KEY_RETURN_VALUE),
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
