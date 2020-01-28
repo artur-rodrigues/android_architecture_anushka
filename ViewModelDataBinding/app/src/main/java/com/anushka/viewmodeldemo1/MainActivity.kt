fe96dceca529d40491ff5dc42f6565c5993ec968
artur.rodrigues <artur.rodrigues@tivit.com>
2020-01-28 13:41:31 -0300

    Commit inicial TwoAwayDemoLiveData

--- ViewModelDataBinding/app/src/main/java/com/anushka/viewmodeldemo1/MainActivity.kt
+++ ViewModelDataBinding/app/src/main/java/com/anushka/viewmodeldemo1/MainActivity.kt
@@ -1,26 +1,28 @@
 package com.anushka.viewmodeldemo1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.ViewModelProvider
 import com.anushka.viewmodeldemo1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
     private lateinit var viewModel: MainActivityViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        viewModel = ViewModelProviders.of(this).get(MainActivityViewModel::class.java)
+        viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
 
-        viewModel.count.observe(this, Observer {
-          binding.countText.text = it.toString()
-        })
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
 
-        binding.button.setOnClickListener {
-           viewModel.updateCount()
-        }
+        /*viewModel.getCount().observe(this, Observer {
+            binding.countText.text = it.toString()
+        })*/
     }
 }
--- ViewModelDataBinding/app/src/main/java/com/anushka/viewmodeldemo1/MainActivityViewModel.kt
+++ ViewModelDataBinding/app/src/main/java/com/anushka/viewmodeldemo1/MainActivityViewModel.kt
@@ -1,16 +1,20 @@
 package com.anushka.viewmodeldemo1
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainActivityViewModel : ViewModel() {
-    var count = MutableLiveData<Int>()
+    private val count = MutableLiveData<Int>()
+    fun getCount(): LiveData<Int> = count
 
     init {
         count.value = 0
     }
 
     fun updateCount(){
-        count.value = (count.value)?.plus(1)
+        count.apply {
+            value = value?.plus(1)
+        }
     }
 }
